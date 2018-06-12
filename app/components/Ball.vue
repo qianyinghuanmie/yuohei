5f7796a67171df3b18a6b435c1b1b9ce9f351726
1029641 <1029641@mochiwang.com>
2018-06-12 18:02:10 +0800

    配置scss

---
+++ app/components/Ball.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="">
+
+  </div>
+</template>
+<script>
+export default {
+  name: "",
+  data: () => ({
+
+  })
+}
+</script>
+<style lang="scss" scoped>
+div{
+  div
+  {
+    
+  }
+}
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,32 +2,25 @@
   <section class="container">
     <div>
       <app-logo/>
-      <h1 class="title">
-        app
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
     </div>
   </section>
 </template>
 
 <script>
 import AppLogo from '~/components/AppLogo.vue'
+import Ball from '~/components/Ball.vue'
 
 export default {
   components: {
     AppLogo
+  },
+  head () {
+    return {
+      title: this.title,
+      meta: [
+        { hid: 'description', name: 'description', content: "starwindxw,个人主站，yyyno" }
+      ]
+    }
   }
 }
 </script>
